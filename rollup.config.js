4925e554af720c4ac553655a508528b9f1c22ba4
lijiayuan <ljy@ljy,com>
2024-12-23 19:00:12 +0800

    feat: 生成产物压缩

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: 'src/index.ts',
@@ -19,6 +20,7 @@ export default {
     resolve(),
     commonjs(),
     typescript(),
+    terser(),
   ],
   external: ['vue-docgen-api']
 };
